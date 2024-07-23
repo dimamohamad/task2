57519f2ef8706cd8d3c33d6a897d6c8d8b3a7dd7
dimamohamad <deemaahmad189@gmail.com>
2024-07-23 22:28:48 +0300

    Add project files.

---
+++ WebApplication1/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            ViewBag.username = Request["name"];
+            ViewBag.phone = Request["phone"];
+            ViewBag.contactmethod = Request["contactMethod"];
+            ViewBag.country = Request["country"];
+            if(!String.IsNullOrEmpty( Request["interests"]))
+            ViewBag.interests = Request["interests"].Split(',');
+
+
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ContactForm()
+        {
+            // Create a list of countries for the dropdown
+            var countries = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "USA", Text = "USA" },
+            new SelectListItem { Value = "UK", Text = "UK" },
+            new SelectListItem { Value = "Canada", Text = "Canada" }
+        };
+
+            // Pass the list of countries to the view via ViewBag
+            ViewBag.CountryList = countries;
+            return View();
+        }
+
+        // POST: /Home/SubmitContactForm
+        [HttpPost]
+        public ActionResult SubmitContactForm()
+        {
+            // Collect form data from Request.Form
+            ViewBag.Name = Request.Form["Name"];
+            ViewBag.Email = Request.Form["Email"];
+            ViewBag.Phone = Request.Form["Phone"];
+            ViewBag.Gender = Request.Form["Gender"];
+            ViewBag.Country = string.Join(", ", Request.Form["Country"]);
+            ViewBag.Message = Request.Form["Message"];
+
+            // Return the result view with the collected data
+            return View("ContactFormResult");
+        }
+    }
+}
